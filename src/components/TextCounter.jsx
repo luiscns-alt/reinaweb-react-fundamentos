a857f997d72494595875ec0be85192d5ce759a27
Luis Carlos <luiscarlosnors@gmail.com>
2021-12-12 20:03:09 -0300

    Feat: Estados

--- src/components/TextCounter.jsx
+++ src/components/TextCounter.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextCounter({ limit }) {
+  const [state, setstate] = useState(5);
   return (
     <div>
+      contador: {state}
       <input type={"text"} maxLength={limit}></input>
     </div>
   );
