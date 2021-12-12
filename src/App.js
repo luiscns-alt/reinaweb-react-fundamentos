e41811d3e4cf53f6b52b25d8f072f65e854a1bfc
Luis Carlos <luiscarlosnors@gmail.com>
2021-12-12 19:45:01 -0300

    Feat: initial react project.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TextCounter from "./components/TextCounter";
+
+function App() {
+  return (
+    <div className="App">
+      <TextCounter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TextCounter.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TextCounter() {
+  return <div>Ola</div>;
+}
