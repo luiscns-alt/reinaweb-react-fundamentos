af6dad980a45ce05620d47128a346f214f9b10e0
Luis Carlos <luiscarlosnors@gmail.com>
2021-12-12 19:49:17 -0300

    Feat: Funtion and Class components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import TextCounter from "./components/TextCounter";
+import TextCounter, { TextCounter2 } from "./components/TextCounter";
 
 function App() {
   return (
     <div className="App">
       <TextCounter />
+      <TextCounter2 />
     </div>
   );
 }
--- src/components/TextCounter.jsx
+++ src/components/TextCounter.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 
 export default function TextCounter() {
   return <div>Ola</div>;
+}
+
+export class TextCounter2 extends Component {
+  render() {
+    return <div>Component Clas</div>;
+  }
 }
